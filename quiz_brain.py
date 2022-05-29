a1cf1ffc5f364a31099fa8291edcd44f60fc84bd
mrdce <71189977+mrdce@users.noreply.github.com>
2022-05-29 20:27:57 +0300

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -5,15 +5,18 @@ class QuizBrain:
         self.score = 0
 
     def next_question(self):
+        ''' Displays the next question'''
         current_question = self.q_list[self.question_number]
         self.question_number += 1
         user_answer = input(f'Q.{self.question_number}. {current_question.text} (True or False): ')
         self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
+        '''Checks if there are questions left'''
         return self.question_number < len(self.q_list)
 
     def check_answer(self, user_answer, correct_answer):
+        '''Checks the user's answer'''
         if user_answer.lower() == correct_answer.lower():
             print('You got it right!')
             self.score += 1
@@ -24,5 +27,6 @@ class QuizBrain:
         print('')
 
     def endgame(self):
+        '''Ends the game and prints scores'''
         print('You have completed the Quiz!')
         print(f'Your final score is {self.score}/{self.question_number}')
